5bc135376ef9e6154962082ea03e68177a81c1e1
JUNG-gyucheol <wjdrbcjf2489@gmail.com>
2024-11-07 15:56:44 +0900

    Remove never used

--- src/components/updateAccount/index.tsx
+++ src/components/updateAccount/index.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useEffect, useMemo, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { InfoType } from "../main";
 
 const UpdateAccount: React.FC<{
